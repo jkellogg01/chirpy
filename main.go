f4e27eec3c4d4d519fd25df4853198af7c33bbe4
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-03-29 18:40:56 -0600

    serve a static file

--- main.go
+++ main.go
@@ -3,25 +3,26 @@ package main
 import "net/http"
 
 func main() {
-    mux := http.NewServeMux()
-    corsMux := middlewareCors(mux)
-    
-    app := http.Server{
-        Addr: ":8080",
-        Handler: corsMux,
-    }
-    app.ListenAndServe()
+	mux := http.NewServeMux()
+	corsMux := middlewareCors(mux)
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	app := http.Server{
+		Addr:    ":8080",
+		Handler: corsMux,
+	}
+	app.ListenAndServe()
 }
 
 func middlewareCors(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-        w.Header().Set("Access-Control-Allow-Headers", "*")
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
