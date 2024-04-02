90b17571ceba61aa547d839e091be7703d7695cb
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-04-02 17:41:47 -0600

    db method: create user

---
+++ internal/database/users.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"cmp"
+	"encoding/json"
+	"slices"
+)
+
+type User struct {
+	Id    int    `json:"id"`
+	Email string `json:"email"`
+}
+
+func (db *DB) CreateUser(email string) (User, error) {
+	data, err := db.readDB()
+	if err != nil {
+		return User{}, err
+	}
+	jsonData := map[string][]User{
+		"users": make([]User, 0),
+	}
+	err = json.Unmarshal(data, jsonData)
+	if err != nil {
+		return User{}, err
+	}
+	users := jsonData["users"]
+	maxId := slices.MaxFunc(users, func(a, b User) int {
+		return cmp.Compare(a.Id, b.Id)
+	}).Id
+	newUser := User{
+		Id:    maxId + 1,
+		Email: email,
+	}
+	users = append(users, newUser)
+	err = db.writeDB(Data{
+		"users": users,
+	})
+	if err != nil {
+		return User{}, err
+	}
+	return newUser, nil
+}
