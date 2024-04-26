b0e7e7efcd4861133718e5b3f556240a4752a0f6
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-04-26 07:35:22 -0600

    model type of revoked token, fetch revoked tokens from database

--- internal/database/tokens.go
+++ internal/database/tokens.go
@@ -1,2 +1,30 @@
 package database
 
+import (
+	"encoding/json"
+	"time"
+)
+
+type RevokedToken struct {
+	Id        string
+	RevokedAt time.Time
+}
+
+func (db *DB) GetRevokedTokens() ([]RevokedToken, error) {
+	data, err := db.readDB()
+	if err != nil {
+		return nil, err
+	}
+	if len(data) == 0 {
+		return []RevokedToken{}, nil
+	}
+	var result struct {
+		Tokens []RevokedToken `json:"tokens"`
+	}
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, err
+	}
+	return result.Tokens, nil
+}
+    
